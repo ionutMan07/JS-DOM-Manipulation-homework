8aa9033def03fc91f134d26178225dcd58c23447
Ionut V <67919936+ionutMan07@users.noreply.github.com>
2020-09-19 12:35:02 +0300

    done

--- app.js
+++ app.js
@@ -1,23 +1,25 @@
-function validate (){
+//  function validate (){
  
-  var firstName = document.getElementById('firstuserinput').value;
-  var secondName = document.getElementById('seconduserinput').value;
-  var gendermen = document.getElementById('Men').checked;
-  var genderwoman = document.getElementById('Woman').checked;
-  var usertext = document.getElementById('usertext').value;
-  var form = [firstName,secondName,gendermen,genderwoman,usertext];
+//    var firstName = document.getElementById('firstuserinput').value;
+//    var secondName = document.getElementById('seconduserinput').value;
+//    var gendermen = document.getElementById('Men').checked;
+//    var genderwoman = document.getElementById('Woman').checked;
+//    var usertext = document.getElementById('usertext').value;
+//    var form = [firstName,secondName,gendermen,genderwoman,usertext];
 
-  for(let char of form){
-  if (firstName==="" || secondName==="" || usertext==="" || gendermen===false && genderwoman===false)
-    {
-      
-     char.style.borderColor = 'red';
-    }
-  document.querySelector(".bar").style.display= "block";
-  document.querySelector("#username").innerText = firstName;
-  //var form = [firstName,secondName,gendermen,genderwoman,usertext];
-  console.log(form);
-  return false;
+//    if (firstName !=='' && secondName !=='' && usertext !==''){
+//       document.querySelector(".bar").style.display= "block";
+//       document.querySelector("#username").innerText = firstName;
+//      }else if (firstName === ''){
+//         document.getElementById('firstuserinput').style.border = "1px solid red";
+//       }else if (secondName ===''){
+//           document.getElementById('seconduserinput').style.borderColor = 'red';
+//       }else if(usertext ===''){
+//           document.getElementById('usertext').style.border = "1px solid red";
+//      }
+//     return false;
+//  }
 
-}
-}
+
+
+
