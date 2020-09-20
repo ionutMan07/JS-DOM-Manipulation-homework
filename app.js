ac228d237af0df172ee563dd2dddf8c72d911752
Ionut V <67919936+ionutMan07@users.noreply.github.com>
2020-09-20 03:59:57 +0300

    done

--- app.js
+++ app.js
@@ -1,25 +1,30 @@
-//  function validate (){
+  function validate (event){
  
-//    var firstName = document.getElementById('firstuserinput').value;
-//    var secondName = document.getElementById('seconduserinput').value;
-//    var gendermen = document.getElementById('Men').checked;
-//    var genderwoman = document.getElementById('Woman').checked;
-//    var usertext = document.getElementById('usertext').value;
-//    var form = [firstName,secondName,gendermen,genderwoman,usertext];
+    var firstName = document.getElementById('firstuserinput').value;
+    var secondName = document.getElementById('seconduserinput').value
+    var usertext = document.getElementById('usertext').value;
+    var username = document.getElementById('username').value;
+    if (firstName === ''){
+        document.getElementById('firstuserinput').classList.add('redBorder');
+    }else if (secondName === ''){
+        document.getElementById('seconduserinput').classList.add('redBorder');
+    }else if (usertext === ''){
+        document.getElementById('usertext').classList.add('redBorder');
+    }
+    else{
+        document.querySelector('.bar').style.display = 'block';
+        document.querySelector('#username').innerText = firstName;
+        document.getElementById('form').reset();
+    }
+}
+ var check = function(){
+    document.getElementById('firstuserinput').classList.remove('redBorder');
+    document.getElementById('seconduserinput').classList.remove('redBorder');
+    document.getElementById('usertext').classList.remove('redBorder');
 
-//    if (firstName !=='' && secondName !=='' && usertext !==''){
-//       document.querySelector(".bar").style.display= "block";
-//       document.querySelector("#username").innerText = firstName;
-//      }else if (firstName === ''){
-//         document.getElementById('firstuserinput').style.border = "1px solid red";
-//       }else if (secondName ===''){
-//           document.getElementById('seconduserinput').style.borderColor = 'red';
-//       }else if(usertext ===''){
-//           document.getElementById('usertext').style.border = "1px solid red";
-//      }
-//     return false;
-//  }
+ }
 
-
-
-
+var btn = document.getElementById('btn');
+btn.addEventListener('click', validate);
+var closeBtn = document.getElementById('close');
+closeBtn.addEventListener('click', function(){window.location.reload()});
